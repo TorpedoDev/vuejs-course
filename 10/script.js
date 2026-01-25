adfa95c2271ce83901c792898422de7988cf463d
Mohammed Fayez <99225851+TorpedoDev@users.noreply.github.com>
2026-01-25 04:25:42 -0800

    Lesson 10 if condition v-if , v-show

---
+++ 10/script.js
@@ -0,0 +1,14 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            name : "Mohammed Fayez",
+            age: 24
+        }
+    },
+
+    methods:{
+
+    }
+}).mount("#app-root")
